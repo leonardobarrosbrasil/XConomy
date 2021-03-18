d505267c8887549ba7d6b18da20d3044884907db
Leonardo Barros <62991452+leonardobarrosbrasil@users.noreply.github.com>
2021-03-18 10:54:05 -0300

    x

---
+++ XConomy-Bukkit/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module XConomy {
+	exports me.yic.xconomy.depend.economyapi;
+	exports me.yic.xconomy.data.sql;
+	exports me.yic.xconomy.depend;
+	exports me.yic.xconomy.command;
+	exports me.yic.xconomy.utils;
+	exports me.yic.xconomy.data.caches;
+	exports me.yic.libs.bstats.sponge;
+	exports me.yic.xconomy.data;
+	exports me.yic.xconomy.lang;
+	exports me.yic.xconomy.task;
+	exports me.yic.xconomy;
+	exports me.yic.xconomy.listeners;
+
+	requires java.sql;
+}
